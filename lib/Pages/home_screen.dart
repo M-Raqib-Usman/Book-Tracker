9bd6a36ec6af46a75d99ba76429f97991c1f6526
M Raqib Usman <raqibusman9@gmail.com>
2025-09-16 01:08:24 +0500

    Showing book search

--- lib/Pages/home_screen.dart
+++ lib/Pages/home_screen.dart
@@ -53,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   Book book=_books[index];
               return ListTile(
                 title: Text(book.title),
-                subtitle: Text(book.authors.join(', &')??''),
+                subtitle: Text(book.authors.join(', &') ?? ''),
               );
             }),
           ),
